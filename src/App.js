aa9dc3d9e06d133f49c01ba1e5a4d758b9941137
M Fahmi Nurul Huda <48041017+mfahminh@users.noreply.github.com>
2022-10-24 20:34:21 +0700

    add daisyui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import './asssests/css/tailwind.css'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex bg-primary">
+      <Sidebar className="flex-1"/>
+      <NavBar className="flex-1"/>
     </div>
   );
 }
