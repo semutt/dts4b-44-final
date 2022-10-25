3ab68e628a46a1257d903a5495dec9355e09d7e4
Semut <44838462+semutt@users.noreply.github.com>
2022-10-25 09:55:49 +0700

    ubah config firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 
 // TODO: replace with your own config
 const firebaseConfig = {
+    // apiKey: "AIzaSyBpugV8U4_hq0RYD8h0WVCnE6kV-p4StBM",
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: "mood-meter-13966.firebaseapp.com",
-    projectId: "mood-meter-13966",
-    storageBucket: "mood-meter-13966.appspot.com",
-    messagingSenderId: "17422836348",
-    appId: "1:17422836348:web:25d2fbcb86bbaddb759ff9",
-    measurementId: "G-YKPBJ9XBLH"
+    authDomain: "mood-meter-3b4c3.firebaseapp.com",
+    projectId: "mood-meter-3b4c3",
+    storageBucket: "mood-meter-3b4c3.appspot.com",
+    messagingSenderId: "524875890329",
+    appId: "1:524875890329:web:94248c250c509385508208",
+    measurementId: "G-M3FWJMCT7N"
 };
 
 const app = initializeApp(firebaseConfig);
